14a3768943c6cabe9c1e2877bc59fab84982ad67
Ivan Necas <inecas@redhat.com>
2013-02-05 11:06:17 +0100

    Extract DSLs to more modules
    Keywords are now separated into this modules:
    
      * Base - defines `_apipie_dsl_data` and other stuff needed for basic
        DSL functionality
    
      * Common - common keywords for resource and action description
    
      * Resource - resource specific keywords
    
      * Action - action specific keywords
    
      * Param - this describes the params, it's in separate because it's
        used in Validators as well

--- lib/apipie/railtie.rb
+++ lib/apipie/railtie.rb
@@ -2,7 +2,10 @@ module Apipie
   class Railtie < Rails::Railtie
     initializer 'apipie.controller_additions' do
       ActiveSupport.on_load :action_controller do
-        extend Apipie::DSL
+        extend Apipie::DSL::Base
+        extend Apipie::DSL::Common
+        extend Apipie::DSL::Action
+        extend Apipie::DSL::Param
       end
     end
   end
