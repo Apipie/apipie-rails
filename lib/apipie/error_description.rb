780ceb0990881185733a4633f1f1e86a1b119cad
Alexander Trigub <altr@ciklum.com>
2013-06-04 18:36:38 +0300

    Changes design errors description, more friendly display

--- lib/apipie/error_description.rb
+++ lib/apipie/error_description.rb
@@ -2,7 +2,7 @@ module Apipie
 
   class ErrorDescription
 
-    attr_reader :code, :description
+    attr_reader :code, :description, :label
 
     def initialize(args)
       if args.first.is_a? Hash
@@ -14,10 +14,34 @@ module Apipie
       end
       @code = args[:code] || args['code']
       @description = args[:desc] || args[:description] || args['desc'] || args['description']
+      @label = get_label
     end
 
     def to_json
-      {:code => code, :description => description}
+      {:code => code, :description => description, :label => label}
+    end
+
+    private
+    
+    def get_label
+      case @code
+        when 200
+          'label label-info'
+        when 201
+          'label label-success'
+        when 204
+          'label label-info2'
+        when 401
+          'label label-warning'
+        when 403
+          'label label-warning2'
+        when 422
+          'label label-important'
+        when 404
+          'label label-inverse'
+        else
+          'label'
+      end
     end
 
   end
