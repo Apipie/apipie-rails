d4d4bac90b1603270602f145cc5f558867f4c90b
Tomas Strachota <tstrachota@redhat.com>
2013-07-19 16:52:52 +0200

    Correct expected_type for parameters taking arrays

---
+++ spec/lib/validator_spec.rb
@@ -0,0 +1,46 @@
+require "spec_helper"
+
+describe Apipie::Validator do
+
+  let(:dsl_data) { ActionController::Base.send(:_apipie_dsl_data_init) }
+
+  let(:resource_desc) do
+    Apipie::ResourceDescription.new(UsersController, "users")
+  end
+
+  let(:method_desc) do
+    Apipie::MethodDescription.new(:show, resource_desc, dsl_data)
+  end
+
+  let(:params_desc) do
+    Apipie::ParamDescription.new(method_desc, :param, nil)
+  end
+
+  describe 'TypeValidator' do
+
+    context "expected type" do
+
+      it "should return hash for type Hash" do
+        validator = Apipie::Validator::TypeValidator.new(params_desc, Hash)
+        validator.expected_type.should == 'hash'
+      end
+
+      it "should return array for type Array" do
+        validator = Apipie::Validator::TypeValidator.new(params_desc, Array)
+        validator.expected_type.should == 'array'
+      end
+
+      it "should return numeric for type Numeric" do
+        validator = Apipie::Validator::TypeValidator.new(params_desc, Numeric)
+        validator.expected_type.should == 'numeric'
+      end
+
+      it "should return string by default" do
+        validator = Apipie::Validator::TypeValidator.new(params_desc, Symbol)
+        validator.expected_type.should == 'string'
+      end
+
+    end
+
+  end
+end
