735a3124a8391002c63928a917da7976b56f1d53
Alexis <alexis@mlp.io>
2013-07-29 13:45:08 -0700

    Add basic authentication

--- lib/apipie/configuration.rb
+++ lib/apipie/configuration.rb
@@ -4,7 +4,7 @@ module Apipie
     attr_accessor :app_name, :app_info, :copyright, :markup, :disqus_shortname,
       :api_base_url, :doc_base_url, :required_by_default, :layout,
       :default_version, :debug, :version_in_url, :namespaced_resources,
-      :validate, :validate_value, :validate_presence
+      :validate, :validate_value, :validate_presence, :username, :password
 
 
     alias_method :validate?, :validate
