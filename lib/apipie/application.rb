5764dd9462c6fb5ec352e036a7c83bc9f5121298
Lior Sion <lior.sion@gmail.com>
2013-08-29 13:36:40 +0300

    typo

--- lib/apipie/application.rb
+++ lib/apipie/application.rb
@@ -80,6 +80,7 @@ module Apipie
     # resource_description? It's used to derivate the default value of
     # versions for methods.
     def controller_versions(controller)
+      binding.pry
       ret = @controller_versions[controller]
       return ret unless ret.empty?
       if controller == ActionController::Base || controller.nil?
@@ -90,6 +91,7 @@ module Apipie
     end
 
     def set_controller_versions(controller, versions)
+      binding.pry
       @controller_versions[controller] = versions
     end
 
@@ -272,6 +274,7 @@ module Apipie
     end
 
     def get_resource_name(klass)
+      binding.pry
       if klass.class == String
         klass
       elsif @controller_to_resource_id.has_key?(klass)
