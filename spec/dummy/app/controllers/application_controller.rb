59db89e60b90c470883448d313e7d6329b0676e2
David Price <dprice@fiksu.com>
2014-11-09 19:46:45 +0200

    Add new configuration.validate value :explicitly

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   
+  before_filter :apipie_validations if ENV['VALIDATE'] == 'explicitly'
+  
   resource_description do
     param :oauth, String, :desc => "Authorization", :required => false
   end
