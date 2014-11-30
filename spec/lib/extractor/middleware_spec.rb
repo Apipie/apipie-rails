a79f59e01e3d6fdfb1c4d2dc15d625ef5b3ceea7
Brad Robel-Forrest <brad@gigglewax.com>
2014-11-30 13:28:23 -0800

    add reformating of rack.request.form_hash for nice multipart example bodies

--- spec/lib/extractor/middleware_spec.rb
+++ spec/lib/extractor/middleware_spec.rb
@@ -18,4 +18,24 @@ describe Apipie::Extractor::Recorder::Middleware do
     expect(Apipie::Extractor).to receive(:clean_call_recorder)
     response
   end
+
+  describe 'with a multipart post' do
+    let(:form_hash) do
+      {
+       'stringbody' => 'this is a string body',
+       'filebody' => {:head => 'X-Fake-Header: fake\r\n'}
+      }
+    end
+
+    let(:response) do
+      request.post('/', 'rack.request.form_hash' => form_hash)
+    end
+
+    it 'reformats form parts' do
+      Apipie.configuration.record = true
+      # expect reformat_multipart_data to invoke content_disposition
+      expect(Apipie::Extractor.call_recorder).to receive(:content_disposition)
+      response
+    end
+  end
 end
