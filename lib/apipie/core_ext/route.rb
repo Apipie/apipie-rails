e915b712ec69627e3504dbf8332c33d5e0e5cca9
Neil Gupta <neil@tabuleapp.com>
2015-06-17 21:18:58 -0500

    Add support for Rails 3 Journey::Routes

--- lib/apipie/core_ext/route.rb
+++ lib/apipie/core_ext/route.rb
@@ -1,3 +1,10 @@
-ActionDispatch::Journey::Route.class_eval do
+begin
+  # Rails 4
+  ActionDispatch::Journey
+rescue NameError
+  # Rails 3
+  require 'journey'
+  Journey
+end::Route.class_eval do
   attr_accessor :base_url
 end
