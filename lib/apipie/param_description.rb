2ceb687a04e1628854e1ea2f53e3fde818458fc4
Josh Pfosi <joshpfosi@gmail.com>
2015-11-10 15:50:58 -0500

    Fixed allow_nil bug

--- lib/apipie/param_description.rb
+++ lib/apipie/param_description.rb
@@ -70,7 +70,7 @@ module Apipie
     end
 
     def validate(value)
-      return true if @allow_nil && value.nil?
+      return true if @allow_nil && value.to_s.blank?
       if (!@allow_nil && value.nil?) || !@validator.valid?(value)
         error = @validator.error
         error = ParamError.new(error) unless error.is_a? StandardError
