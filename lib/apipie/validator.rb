81bc67eda3fa123da234ac27924b109a4ada712d
Andrew Hunter <hunteraa@gmail.com>
2016-02-21 12:20:13 -0600

    adding DecimalValidator

--- lib/apipie/validator.rb
+++ lib/apipie/validator.rb
@@ -374,6 +374,27 @@ module Apipie
       end
     end
 
+    class DecimalValidator < BaseValidator
+
+      def validate(value)
+        self.class.validate(value)
+      end
+
+      def self.build(param_description, argument, options, block)
+        if argument == :decimal
+          self.new(param_description)
+        end
+      end
+
+      def description
+        "Must be a decimal number."
+      end
+
+      def self.validate(value)
+        value.to_s =~ /\A[0-9]+([,.][0-9]+)?\Z$/
+      end
+    end
+
     class NumberValidator < BaseValidator
 
       def validate(value)
