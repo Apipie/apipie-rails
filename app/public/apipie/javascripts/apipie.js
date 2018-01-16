a3fa129783d382be6b918b82b793702f76a0207c
Paul Martensen <paul.martensen@gmx.de>
2018-01-16 10:39:23 +0100

    Removed obsolete js.

--- app/public/apipie/javascripts/apipie.js
+++ app/public/apipie/javascripts/apipie.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-  $('#details a').click(function(e) {
-    e.preventDefault();
-    $(this).tab('show')
-  });
-
   if (typeof prettyPrint == 'function') {
     $('pre.ruby').addClass('prettyprint lang-rb');
     prettyPrint();
