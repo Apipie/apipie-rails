103d39538f879e86cb3230da7ec19d71dd933f53
Derek Wheel <dwheel@instructure.com>
2019-09-19 14:46:15 -0700

    Update gemfiles with test engine and refactor

--- lib/apipie/core_ext/route.rb
+++ lib/apipie/core_ext/route.rb
@@ -1,10 +1,9 @@
-begin
-  # Rails 4
-  ActionDispatch::Journey
-rescue NameError
-  # Rails 3
-  require 'journey'
-  Journey
-end::Route.class_eval do
-  attr_accessor :base_url
+module Apipie
+  module BaseUrlExtension
+    attr_accessor :base_url
+  end
+end
+
+class ActionDispatch::Journey::Route
+  include Apipie::BaseUrlExtension
 end
