243e2824ea4bbc505e48ac072b163b977321a851
Derek Wheel <dwheel@instructure.com>
2019-09-19 15:33:25 -0700

    remove rails 3 handling from core_ext/route

--- lib/apipie/core_ext/route.rb
+++ lib/apipie/core_ext/route.rb
@@ -1,10 +1,3 @@
-begin
-  # Rails 4
-  ActionDispatch::Journey
-rescue NameError
-  # Rails 3
-  require 'journey'
-  Journey
-end::Route.class_eval do
+ActionDispatch::Journey::Route.class_eval do
   attr_accessor :base_url
 end
