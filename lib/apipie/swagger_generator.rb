76a4ca75ec5e720951624a6fea94b40ba6054007
Sean Nichols <sean.nichols@instacart.com>
2020-08-20 09:47:34 -0400

    Prevent null refs in Swagger gen

--- lib/apipie/swagger_generator.rb
+++ lib/apipie/swagger_generator.rb
@@ -640,7 +640,12 @@ module Apipie
 
       if params_in_body? && body_allowed_for_current_method
         if params_in_body_use_reference?
-          swagger_schema_for_body = {"$ref" => gen_referenced_block_from_params_array("#{swagger_op_id_for_method(method)}_input", body_param_defs_array)}
+          referenced_block = gen_referenced_block_from_params_array("#{swagger_op_id_for_method(method)}_input", body_param_defs_array)
+          if referenced_block.present?
+            swagger_schema_for_body = {"$ref" => referenced_block}
+          else
+            swagger_schema_for_body = nil
+          end
         else
           swagger_schema_for_body = json_schema_obj_from_params_array(body_param_defs_array)
         end
