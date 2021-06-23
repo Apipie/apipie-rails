4d2c3e6b7758e85abdc37070c946df3289af08f2
Emmett Underhill <emmett.underhill@instacart.com>
2021-06-23 01:17:02 -0400

    make uniqe param names optional

--- lib/apipie/swagger_generator.rb
+++ lib/apipie/swagger_generator.rb
@@ -595,8 +595,7 @@ module Apipie
     def ref_name_from_param_desc(param_desc, name_fallback = '')
       op_id = swagger_op_id_for_path(param_desc.method_description.apis.first.http_method,
                                      param_desc.method_description.apis.first.path)
-      part_of_one_of = param_desc.parents_and_self.any? { |pd| swagger_param_type(pd) == 'oneOf' }
-      name = part_of_one_of ? param_desc.full_path.join('_') : param_desc.name
+      name = param_desc.options[:unique_param_names] ? param_desc.full_path.join('_') : param_desc.name
       "#{op_id}_param_#{name || name_fallback}"
     end
 
