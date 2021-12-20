9f10af432f3c61357f3e1729906e1f1ba249686c
Frank Hock <frank.hock@gmail.com>
2021-12-20 14:42:32 -0500

    fix schema generation

--- lib/apipie/swagger_generator.rb
+++ lib/apipie/swagger_generator.rb
@@ -476,6 +476,9 @@ module Apipie
 
       if swagger_def[:type] == "array"
         swagger_def[:items] = {type: "string"}
+        enum = param_desc.options.fetch(:in, [])
+
+        swagger_def[:items][:enum] = enum if enum.any?
       end
 
       if swagger_def[:type] == "enum"
@@ -504,7 +507,8 @@ module Apipie
       end
 
       if !in_schema
-        swagger_def[:in] = param_desc.options.fetch(:in, @default_value_for_param_in)
+        # the "name" and "in" keys can only be set on root parameters (non-nested)
+        swagger_def[:in] = @default_value_for_param_in if name.present?
         swagger_def[:required] = param_desc.required if param_desc.required
       end
 
