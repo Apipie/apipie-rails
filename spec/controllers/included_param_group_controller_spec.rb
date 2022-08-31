c250cc2033d230a38b12f4a4513910f0dc7118fa
olliswe <oliver.iyer@doctolib.com>
2022-08-31 11:33:10 +0200

    fix indentation

--- spec/controllers/included_param_group_controller_spec.rb
+++ spec/controllers/included_param_group_controller_spec.rb
@@ -5,9 +5,9 @@ describe IncludedParamGroupController do
 
   let(:dsl_data) { ActionController::Base.send(:_apipie_dsl_data_init) }
 
+  it "should not error when there is a param_group that is deeply nested in response description" do
+    subject = Apipie.get_resource_description(IncludedParamGroupController, Apipie.configuration.default_version)
+    expect(subject._methods.keys).to include(:show)
+  end
 
-    it "should not error when there is a param_group that is deeply nested in response description" do
-      subject = Apipie.get_resource_description(IncludedParamGroupController, Apipie.configuration.default_version)
-      expect(subject._methods.keys).to include(:show)
-    end
 end
