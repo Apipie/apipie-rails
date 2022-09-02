9ef91aea2d723f777b7794e7e26994080d9284c2
Mathieu Jobin <mathieu@justbudget.com>
2022-09-03 08:38:30 +0900

    redo config/boot file in dummy

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,10 +1,6 @@
 require 'rubygems'
-gemfile = File.expand_path('../../../../Gemfile', __FILE__)
 
-if File.exist?(gemfile)
-  ENV['BUNDLE_GEMFILE'] = gemfile
-  require 'bundler'
-  Bundler.setup
-end
+require 'bundler'
+Bundler.setup
 
 $:.unshift File.expand_path('../../../../lib', __FILE__)
