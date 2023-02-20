114960f66a599096e28f48574e5bf0cd0d48feb9
Mathieu Jobin <99191+mathieujobin@users.noreply.github.com>
2023-02-20 20:59:54 +0900

    fix current offenses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,7 +55,7 @@ end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[File.expand_path('support/**/*.rb', __dir__)].sort.each {|f| require f}
+Dir[File.expand_path('support/**/*.rb', __dir__)].each {|f| require f}
 
 RSpec.configure do |config|
 
