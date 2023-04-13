50f18a07ecf223082b8bd20e6f3b554b1d0066ea
Panos Dalitsouris <hello@panos.codes>
2023-04-13 11:21:49 +0300

    Refactor MethodDescription specs

--- spec/lib/apipie/method_description_spec.rb
+++ spec/lib/apipie/method_description_spec.rb
@@ -1,98 +1,127 @@
 require "spec_helper"
 
 describe Apipie::MethodDescription do
-
   let(:dsl_data) { ActionController::Base.send(:_apipie_dsl_data_init) }
+  let(:deprecated_resource_description) { false }
+  let(:method_description_method) { :some_method }
 
-  describe "metadata" do
-
-    before(:each) do
-      @resource = Apipie::ResourceDescription.new(ApplicationController, "dummy")
-    end
-
-    it "should return nil when no metadata is provided" do
-      method = Apipie::MethodDescription.new(:a, @resource, dsl_data)
-      expect(method.to_json[:metadata]).to eq(nil)
-    end
-
-    it "should return the metadata" do
-      meta = {
-        :lenght => 32,
-        :weight => '830g'
+  let(:resource_description_dsl) do
+    ActionController::Base.send(:_apipie_dsl_data_init).merge(
+      {
+        deprecated: deprecated_resource_description
       }
-      method = Apipie::MethodDescription.new(:a, @resource, dsl_data.update(:meta => meta))
-      expect(method.to_json[:metadata]).to eq(meta)
-    end
-
+    )
   end
 
-  describe "deprecated flag" do
-    before(:each) do
-      @resource = Apipie::ResourceDescription.new(ApplicationController, "dummy")
+  let(:resource_description) do
+    Apipie::ResourceDescription.new(
+      ApplicationController,
+      'dummy',
+      resource_description_dsl
+    )
+  end
+
+  let(:method_description) do
+    Apipie::MethodDescription.new(
+      method_description_method,
+      resource_description,
+      dsl_data
+    )
+  end
+
+  describe '#to_json' do
+    describe 'metadata' do
+      subject { method_description.to_json[:metadata] }
+
+      it { is_expected.to be_nil }
+
+      context 'when meta are given' do
+        let(:meta) { { length: 32, weight: '830g' } }
+        let(:dsl_data) { super().merge(meta: meta) }
+
+        it { is_expected.to eq(meta) }
+      end
     end
 
-    it "should return the deprecated flag when provided" do
-      dsl_data[:api_args] = [[:GET, "/foo/bar", "description", {:deprecated => true}]]
-      method = Apipie::MethodDescription.new(:a, @resource, dsl_data)
-      expect(method.method_apis_to_json.first[:deprecated]).to eq(true)
-    end
+    describe 'params' do
+      subject(:json_params) { method_description.to_json[:params].map { |h| h[:name] } }
 
-    it "should return the deprecated flag if resource is deprecated" do
-      @resource.instance_variable_set("@_deprecated", true)
-      dsl_data[:api_args] = [[:GET, "/foo/bar", "description", {}]]
-      method = Apipie::MethodDescription.new(:a, @resource, dsl_data)
-      expect(method.method_apis_to_json.first[:deprecated]).to eq(true)
+      let(:dsl_data) do
+        super().merge(
+          {
+            params: [
+              [:a, String, nil, {}, nil],
+              [:b, String, nil, {}, nil],
+              [:c, String, nil, {}, nil]
+            ]
+          }
+        )
+      end
+
+      it 'should be ordered' do
+        expect(json_params).to eq(%w[a b c])
+      end
+
+      context 'when param is only for response' do
+        let(:dsl_data) do
+          super().merge(
+            {
+              params: [
+                [:a, String, nil, { only_in: :request }, nil],
+                [:b, String, nil, { only_in: :response }, nil],
+                [:c, String, nil, {}, nil]
+              ]
+            }
+          )
+        end
+
+        it 'should ignore response-only parameters' do
+          expect(json_params).to eq(%w[a c])
+        end
+      end
     end
   end
 
-  describe "params descriptions" do
+  describe '#method_apis_to_json' do
+    describe 'deprecated' do
+      subject { method_description.method_apis_to_json.first[:deprecated] }
 
-    before(:each) do
-      @resource = Apipie::ResourceDescription.new(ApplicationController, "dummy")
-      dsl_data[:params] = [[:a, String, nil, {}, nil],
-                           [:b, String, nil, {}, nil],
-                           [:c, String, nil, {}, nil]]
-      @method = Apipie::MethodDescription.new(:a, @resource, dsl_data)
-      @resource.add_method_description @method
-    end
+      let(:api_deprecated) { false }
+      let(:dsl_data) { super().merge(api_args: api_args) }
 
-    it "should be ordered" do
-      expect(@method.params.keys).to eq([:a, :b, :c])
-      expect(@method.to_json[:params].map { |h| h[:name] }).to eq(%w[a b c])
-    end
+      let(:api_args) do
+        [[:GET, "/foo/bar", "description", { deprecated: api_deprecated }]]
+      end
 
-    it "should be still ordered" do
-      expect(@method.params.keys).to eq([:a, :b, :c])
-      expect(@method.to_json[:params].map { |h| h[:name] }).to eq(%w[a b c])
-    end
+      it { is_expected.to eq(false) }
 
-  end
+      context 'when api is deprecated' do
+        let(:api_deprecated) { true }
 
-  describe "response-only properties" do
-    before(:each) do
-      @resource = Apipie::ResourceDescription.new(ApplicationController, "dummy")
-      dsl_data[:params] = [[:a, String, nil, {:only_in => :request}, nil],
-                           [:b, String, nil, {:only_in => :response}, nil],
-                           [:c, String, nil, {}, nil]]
-      @method = Apipie::MethodDescription.new(:a, @resource, dsl_data)
-      @resource.add_method_description @method
-    end
+        it { is_expected.to eq(true) }
+      end
 
-    it "should ignore response-only parameters" do
-      expect(@method.params.keys).to eq([:a, :c])
-      expect(@method.to_json[:params].map { |h| h[:name] }).to eq(%w[a c])
+      context 'when resource description is deprecated' do
+        let(:deprecated_resource_description) { true }
+
+        it { is_expected.to eq(true) }
+      end
     end
   end
 
+  describe '#returns' do
+    subject(:method_desc) { method_description }
 
-  describe "'returns' properties" do
-    it "should raise an error if both :param_group and :array_of are specified in 'returns'" do
-      @resource = Apipie::ResourceDescription.new(ApplicationController, "dummy")
-      dsl_data[:returns] = { 200 => [{:param_group => 'pet', :array_of => 'pet'}, nil, nil] }
+    context 'when both :param_group and :array_of are specified' do
+      let(:returns) do
+        { 200 => [{ param_group: 'pet', array_of: 'pet' }, nil, nil] }
+      end
 
-      expect { Apipie::MethodDescription.new(:a, @resource, dsl_data) }.to raise_error(Apipie::ReturnsMultipleDefinitionError)
+      let(:dsl_data) { super().merge({ returns: returns }) }
+
+      it 'raises an error' do
+        expect { method_desc }.to raise_error(Apipie::ReturnsMultipleDefinitionError)
+      end
     end
   end
-
-
 end
