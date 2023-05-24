Apipie/apipie-rails

commit 9b2da98262f4d1e5c95bbf05a894cc3fc7648358

Allow resource_name to be inherited (#872)
This is helpful if you have a very structured hierarchy of API controllers and would like to define the naming logic once in a base controller.

* Allow resource_name to be a string, symbol, or proc
* Search all the way up the controller hierarchy for a value
* Continue searching even if a particular controller doesn't have a `resource_description` block

This is helpful if you have a very structured hierarchy of API controllers and would like to define the naming logic once in a base controller.

* Fix whitespace on README

This was causing the test commands not to show up on Github

* Set Rubocop target Ruby version to 2.6

I had to set Rubocop's target Ruby version to 2.6 in order to get a bunch of offenses to go away. I'm not sure why that suddenly started happening (my PR from last week didn't have any issues).

lib/apipie/application.rb
@@ -122,13 +122,24 @@ module Apipie
     # resource_description? It's used to derivate the default value of
     # versions for methods.
     def controller_versions(controller)
-      ret = @controller_versions[controller.to_s]
-      return ret unless ret.empty?
-      if controller == ActionController::Base || controller.nil?
-        return [Apipie.configuration.default_version]
-      else
-        return controller_versions(controller.to_s.constantize.superclass)
+      value_from_parents(controller, default: [Apipie.configuration.default_version]) do |c|
+        ret = @controller_versions[c.to_s]
+        ret unless ret.empty?
       end
+    end
+
+    # Recursively walks up the controller hierarchy looking for a value
+    # from the block.
+    # Stops at ActionController::Base.
+    # @param [Class] controller controller to start from
+    # @param [Array] args arguments passed to the block
+    # @param [Object] default default value to return if no value is found
+    # @param [Proc] block block to call with controller and args
+    def value_from_parents(controller, *args, default: nil, &block)
+      return default if controller == ActionController::Base || controller == AbstractController::Base || controller.nil?
+
+      thing = yield(controller, *args)
+      thing || value_from_parents(controller.superclass, *args, default: default, &block)
     end
 
     def set_controller_versions(controller, versions)

lib/apipie/resource_description.rb
@@ -26,7 +26,9 @@ module Apipie
       @controller = controller
       @_id = id
       @_version = version || Apipie.configuration.default_version
-      @_parent = Apipie.get_resource_description(controller.superclass, version)
+      @_parent = Apipie.value_from_parents(controller.superclass, version) do |parent, ver|
+        Apipie.get_resource_description(parent, ver)
+      end
 
       update_from_dsl_data(dsl_data) if dsl_data
     end
@@ -60,7 +62,16 @@ module Apipie
     end
 
     def name
-      @name ||= @_resource_name.presence || @_id.split('-').map(&:capitalize).join('::')
+      @name ||= case resource_name
+                when Proc
+          resource_name.call(controller)
+                when Symbol
+          controller.public_send(resource_name)
+                when String
+          resource_name
+        else
+          default_name
+        end
     end
     alias _name name
 
@@ -121,6 +132,18 @@ module Apipie
         :headers => _headers,
         :deprecated => @_deprecated
       }
+    end
+
+    protected
+
+    def resource_name
+      @_resource_name.presence || @_parent&.resource_name
+    end
+
+    private
+
+    def default_name
+      @_id.split('-').map(&:capitalize).join('::')
     end
 
   end

spec/dummy/app/controllers/api/v2/empty_middle_controller.rb
@@ -1,0 +1,9 @@
+module Api
+  module V2
+    class EmptyMiddleController < V2::BaseController
+      # This is an empty controller, used to test cases where controllers
+      # may inherit from a middle controler that does not define a resource_description,
+      # but the middle controller's parent does.
+    end
+  end
+end

.rubocop.yml
@@ -10,6 +10,7 @@ Rails:
 
 AllCops:
   NewCops: enable
+  TargetRubyVersion: 2.6
 
 Layout/LineLength:
   Max: 140
@@ -69,43 +70,43 @@ Style/HashSyntax:
 
 Metrics/ParameterLists:
   Exclude:
-    - 'lib/apipie/generator/swagger/context.rb'
+    - "lib/apipie/generator/swagger/context.rb"
 
 Style/Documentation:
   Exclude:
-    - 'app/controllers/apipie/apipies_controller.rb'
-    - 'app/helpers/apipie_helper.rb'
-    - 'lib/apipie/apipie_module.rb'
-    - 'lib/apipie/application.rb'
-    - 'lib/apipie/configuration.rb'
-    - 'lib/apipie/core_ext/route.rb'
-    - 'lib/apipie/dsl_definition.rb'
-    - 'lib/apipie/error_description.rb'
-    - 'lib/apipie/errors.rb'
-    - 'lib/apipie/extractor.rb'
-    - 'lib/apipie/extractor/collector.rb'
-    - 'lib/apipie/extractor/recorder.rb'
-    - 'lib/apipie/extractor/writer.rb'
-    - 'lib/apipie/generator/generator.rb'
-    - 'lib/apipie/generator/swagger/**/*'
-    - 'lib/apipie/helpers.rb'
-    - 'lib/apipie/markup.rb'
-    - 'lib/apipie/method_description.rb'
-    - 'lib/apipie/method_description/api.rb'
-    - 'lib/apipie/middleware/checksum_in_headers.rb'
-    - 'lib/apipie/railtie.rb'
-    - 'lib/apipie/response_description.rb'
-    - 'lib/apipie/response_description_adapter.rb'
-    - 'lib/apipie/routes_formatter.rb'
-    - 'lib/apipie/routing.rb'
-    - 'lib/apipie/rspec/response_validation_helper.rb'
-    - 'lib/apipie/swagger_generator.rb'
-    - 'lib/apipie/see_description.rb'
-    - 'lib/apipie/static_dispatcher.rb'
-    - 'lib/apipie/tag_list_description.rb'
-    - 'lib/apipie/validator.rb'
-    - 'lib/generators/apipie/install/install_generator.rb'
-    - 'lib/generators/apipie/views_generator.rb'
+    - "app/controllers/apipie/apipies_controller.rb"
+    - "app/helpers/apipie_helper.rb"
+    - "lib/apipie/apipie_module.rb"
+    - "lib/apipie/application.rb"
+    - "lib/apipie/configuration.rb"
+    - "lib/apipie/core_ext/route.rb"
+    - "lib/apipie/dsl_definition.rb"
+    - "lib/apipie/error_description.rb"
+    - "lib/apipie/errors.rb"
+    - "lib/apipie/extractor.rb"
+    - "lib/apipie/extractor/collector.rb"
+    - "lib/apipie/extractor/recorder.rb"
+    - "lib/apipie/extractor/writer.rb"
+    - "lib/apipie/generator/generator.rb"
+    - "lib/apipie/generator/swagger/**/*"
+    - "lib/apipie/helpers.rb"
+    - "lib/apipie/markup.rb"
+    - "lib/apipie/method_description.rb"
+    - "lib/apipie/method_description/api.rb"
+    - "lib/apipie/middleware/checksum_in_headers.rb"
+    - "lib/apipie/railtie.rb"
+    - "lib/apipie/response_description.rb"
+    - "lib/apipie/response_description_adapter.rb"
+    - "lib/apipie/routes_formatter.rb"
+    - "lib/apipie/routing.rb"
+    - "lib/apipie/rspec/response_validation_helper.rb"
+    - "lib/apipie/swagger_generator.rb"
+    - "lib/apipie/see_description.rb"
+    - "lib/apipie/static_dispatcher.rb"
+    - "lib/apipie/tag_list_description.rb"
+    - "lib/apipie/validator.rb"
+    - "lib/generators/apipie/install/install_generator.rb"
+    - "lib/generators/apipie/views_generator.rb"
     - spec/support/custom_bool_validator.rb
     - spec/lib/validators/array_validator_spec.rb
     - spec/dummy/**/*.rb
@@ -122,6 +123,6 @@ Naming/BlockForwarding:
 
 Lint/MissingSuper:
   Exclude:
-    - 'lib/apipie/errors.rb'
-    - 'lib/apipie/response_description_adapter.rb'
-    - 'lib/apipie/validator.rb'
+    - "lib/apipie/errors.rb"
+    - "lib/apipie/response_description_adapter.rb"
+    - "lib/apipie/validator.rb"

spec/controllers/api/v2/nested/resources_controller_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper'
 
 describe Api::V2::Nested::ResourcesController do
+  let(:resource_description) { Apipie.get_resource_description(described_class, "2.0") }
+
+  describe "resource description" do
+    describe 'version' do
+      subject { resource_description._version }
+
+      it { is_expected.to eq('2.0') }
+    end
+
+    describe 'name' do
+      subject { resource_description.name }
+
+      it { is_expected.to eq('Rsrcs') }
+    end
+  end
+
   describe '.get_resource_id' do
     subject { Apipie.get_resource_id(Api::V2::Nested::ResourcesController) }
 

spec/controllers/api/v2/sub/footguns_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe Api::V2::ArchitecturesController do
-  let(:resource_description) { Apipie.get_resource_description(described_class, "2.0") }
+describe Api::V2::Sub::FootgunsController do
+  let(:resource_description) { Apipie.get_resource_description(described_class, '2.0') }
 
-  describe "resource description" do
+  describe 'resource description' do
     describe 'version' do
       subject { resource_description._version }
 
@@ -13,7 +13,7 @@ describe Api::V2::ArchitecturesController do
     describe 'name' do
       subject { resource_description.name }
 
-      it { is_expected.to eq('Architectures') }
+      it { is_expected.to eq('snugtooF') }
     end
   end
 end

spec/dummy/app/controllers/api/v2/base_controller.rb
@@ -5,6 +5,12 @@ module Api
         api_version '2.0'
         app_info 'Version 2.0 description'
         api_base_url '/api/v2'
+
+        name :reversed_name
+      end
+
+      def self.reversed_name
+        controller_name.capitalize.reverse
       end
     end
   end

spec/dummy/app/controllers/api/v2/nested/resources_controller.rb
@@ -1,8 +1,8 @@
 module Api
   module V2
     class Nested::ResourcesController < V2::BaseController
-      resource_description do 
-        name 'Resources'
+      resource_description do
+        name ->(controller) { controller.controller_name.delete('aeiou').capitalize }
         resource_id "resource"
       end
       api :GET, "/nested/resources/", "List all nested resources."

commit 39297616beae88c143690676617208e0dfa30725

Fix error climbing controller hierarchy
When using namespaced resources, climbing the controller hierarchy was broken in
really specific circumstances. Bug introduced in https://github.com/Apipie/apipie-rails/pull/872

lib/apipie/application.rb
@@ -454,7 +454,7 @@ module Apipie
     end
 
     def version_prefix(klass)
-      version = controller_versions(klass.to_s).first
+      version = controller_versions(klass).first
       base_url = get_base_url(version)
       return "/" if base_url.blank?
       base_url[1..-1] + "/"

lib/apipie/resource_description.rb
@@ -101,7 +101,9 @@ module Apipie
       Apipie.full_url crumbs.join('/')
     end
 
-    def api_url; "#{Apipie.api_base_url(_version)}#{@_path}"; end
+    def api_url
+      "#{Apipie.api_base_url(_version)}#{@_path}"
+    end
 
     def valid_method_name?(method_name)
       @_methods.keys.map(&:to_s).include?(method_name.to_s)

spec/controllers/api/v2/empty_middle_controller_spec.rb
@@ -1,0 +1,23 @@
+require 'spec_helper'
+
+describe Api::V2::EmptyMiddleController do
+  let(:resource_description) { Apipie.get_resource_description(described_class, '2.0') }
+
+  describe 'resource description' do
+    subject { resource_description }
+
+    context 'when namespaced resources are enabled' do
+      before { Apipie.configuration.namespaced_resources = true }
+      after { Apipie.configuration.namespaced_resources = false }
+
+      # we don't actually expect the resource description to be nil, but resource IDs
+      # are computed at file load time, and altering the value of namespaced_resources
+      # after the fact doesn't change the resource ID, so it can't be found
+      it { is_expected.to be_nil }
+    end
+
+    context 'when namespaced resources are disabled' do
+      it { is_expected.to be_nil }
+    end
+  end
+end

spec/dummy/app/controllers/api/v2/empty_middle_controller.rb
@@ -4,6 +4,11 @@ module Api
       # This is an empty controller, used to test cases where controllers
       # may inherit from a middle controler that does not define a resource_description,
       # but the middle controller's parent does.
+
+      def inconsequential_method
+        # This method is here to ensure that the controller is not empty.
+        # It triggers method_added, which is used to add the resource description.
+      end
     end
   end
 end